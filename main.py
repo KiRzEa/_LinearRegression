faab4979b809b28d6bdc84c32be06ef6e3362512
KiRzEa <21521042@gm.uit.edu.vn>
2022-09-19 11:22:35 +0700

    .

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from functions import *
+from functions import*
 df = pd.read_csv('USA_Housing.csv')
 df_X = df[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms', 'Avg. Area Number of Bedrooms', 'Area Population']]
 df_Y = df['Price']
 X = np.ones((np.size(df_X, 0), np.size(df_X, 1) + 1))
-X[:, 1:] = np.array(df_X.values, dtype=np.float64)
+X[:, 1:] = np.array(df_X.values, 'float')
 y = np.array(df_Y.values, 'float')
-Theta = GradientDescent(X[:3000, :], y[:3000])
+Theta = GradientDescent(X, y)
+_Theta = GradientDescent_1(X, y)
+print(Theta, "\n", _Theta)
