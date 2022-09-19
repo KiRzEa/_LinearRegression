94192e3bec4050e0fa8443d2cf047bdb58cf9518
KiRzEa <21521042@gm.uit.edu.vn>
2022-09-19 13:28:30 +0700

    .

--- functions.py
+++ functions.py
@@ -2,26 +2,25 @@ import numpy as np
 def predict(X, Theta):
     return X @ Theta
 def computeCost(X, y, Theta):
-    sqr_error = (predict(X, Theta) - y) ** 2
+    error = predict(X, Theta) - y
     m = np.size(y)
-    J = 1.0/(2*m) * np.sum(sqr_error)
+    J = (np.transpose(error) @ error) / (2 * m)
     return J
-def GradientDescent(X, y, alpha=0.00001, iter=10000):
+def normalize(X):
+    return (X - np.mean(X)) / (np.std(X))
+def GradientDescent(X, y, alpha=0.01, iter=10000):
     Theta = np.zeros(np.size(X, 1))
     pre_cost = computeCost(X, y, Theta)
-    X_T = np.transpose(X[:, 1:])
+    X_T = np.transpose(X)
     m = np.size(y)
     for i in range(0, iter):
         error = predict(X, Theta) - y
-        tmp_b = Theta[0] - (alpha / m) * (np.sum(X[:, 0] @ error))
-        tmp_w = Theta[1:] - (alpha / m) * (X_T @ error)
-        Theta[0] = tmp_b
-        Theta[1:] = tmp_w
+        Theta = Theta - (alpha / m) * (X_T @ error)
         cost = computeCost(X, y, Theta)
         if (np.round(cost, 15) == np.round(pre_cost, 15)):
             break
         pre_cost = cost
     return Theta
-def GradientDescent_1(X, y):
+def NormalEquation(X, y):
     Theta = np.linalg.inv(np.transpose(X) @ X) @ np.transpose(X) @ y
     return Theta
