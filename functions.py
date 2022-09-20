d4ae3b9c5abea669d69cfbec7cac5b8af9299bf7
KiRzEa <21521042@gm.uit.edu.vn>
2022-09-20 09:18:18 +0700

    .

--- functions.py
+++ functions.py
@@ -6,10 +6,8 @@ def computeCost(X, y, Theta):
     m = np.size(y)
     J = (np.transpose(error) @ error) / (2 * m)
     return J
-def normalize(X):
-    return (X - np.mean(X)) / (np.std(X))
-def GradientDescent(X, y, alpha=0.01, iter=10000):
-    Theta = np.zeros(np.size(X, 1))
+def GradientDescent(X, y, alpha=0.000000000337, iter=5000):
+    Theta = np.zeros(np.size(X, 1), dtype=np.float64)
     pre_cost = computeCost(X, y, Theta)
     X_T = np.transpose(X)
     m = np.size(y)
@@ -18,6 +16,7 @@ def GradientDescent(X, y, alpha=0.01, iter=10000):
         Theta = Theta - (alpha / m) * (X_T @ error)
         cost = computeCost(X, y, Theta)
         if (np.round(cost, 15) == np.round(pre_cost, 15)):
+            print(Theta)
             break
         pre_cost = cost
     return Theta
--- main.py
+++ main.py
@@ -7,6 +7,6 @@ df_Y = df['Price']
 X = np.ones((np.size(df_X, 0), np.size(df_X, 1) + 1))
 X[:, 1:] = np.array(df_X.values, 'float')
 y = np.array(df_Y.values, 'float')
-Theta = GradientDescent(X, y)
-_Theta = NormalEquation(X, y)
+Theta = GradientDescent(X[:4000, :], y[:4000])
+_Theta = NormalEquation(X[:4000, :], y[:4000])
 print(Theta, "\n", _Theta)
